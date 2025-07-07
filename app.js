1fd83a7e7b210b4c825f75be8f8fb8def3cd23d2
Ansa Ghani <ansaghani74@gmail.com>
2025-07-07 10:00:10 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var JhonDoe = "JhonDoe"
+alert(JhonDoe);
+
+var age = "15 years old"
+alert(age);
+
+var certified = "Certified Mobile Application Development"
+alert(certified);
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza);
+
+var email = "My email address is example@gmail.com"
+alert(email);
+
+var smartwaytolearnjavascript = "I am trying to learn the book way to learn javascript"
+alert(smartwaytolearnjavascript);
+
+var emoji = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(emoji);
